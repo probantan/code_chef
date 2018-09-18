eb324a2631198eceea171242322efe34737c5810
Protus bantan <protus.bantan@gmail.com.com>
2018-09-18 11:17:38 +0300

    Adds schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 bootstrap = Bootstrap()
-
+db = SQLAlchemy()
+login_manager = LoginManager()
 def create_app(config_name):
     app = Flask(__name__)
 
@@ -12,10 +14,11 @@ def create_app(config_name):
     '''
      #creating the app configurations
     app.config.from_object(config_options[config_name])
-
+    db.init_app(app)
     #initializing the flask extensions
     bootstrap.init_app(app)
 
+    login_manager.init_app(app)
     #Registering the bluprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
---
+++ app/models.py
@@ -0,0 +1,39 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
+    projects = db.relationship("Projects", backref="user", lazy="dynamic")
+    comment = db.relationship("Comments", backref="user", lazy ="dynamic")
+
+class Role(db.Model):
+       __tablename__ = 'roles'
+       id = db.Column(db.Integer,primary_key = True)
+       name = db.Column(db.String(255))
+       users = db.relationship('User',backref = 'role',lazy="dynamic")
+       
+       
+       def __repr__(self):
+           return f'User {self.name}'
+
+class Projects(db.Model):
+    '''
+    defines the table instance of our projects table
+    '''
+    __tablename__= 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    Blog_post = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
